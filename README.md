0cb5fd96cd319c51992d233c216172f07673436e
Shcoobz <a.boeckmann23@gmail.com>
2024-03-14 09:42:14 +0100

    refactored code

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# custom-countdown
+# [LIVE DEMO](https://shcoobz.github.io/custom-countdown/) -> Custom Countdown
+
+![Project Image](img/custom-countdown.png)
+
+## Summary
+
+Welcome to Custom Countdown! This application allows users to create and customize countdowns for various events or occasions. It features a user-friendly interface and essential functionalities for managing countdowns.
+
+The core functionality includes:
+
+- Creation of custom countdowns by specifying a title and selecting a date.
+- Real-time countdown display showing days, hours, minutes, and seconds remaining until the event.
+- Option to reset the countdown or start a new one after completion.
+
+## Features
+
+### Countdown Creation
+
+Users can create custom countdowns by entering a title for the event and selecting a date using the date picker.
+
+### Real-time Countdown Display
+
+The application provides a real-time countdown display, updating every second to show the remaining time until the specified event.
+
+### Countdown Reset
+
+Users can reset the countdown at any time, clearing the current countdown and allowing them to set a new one.
+
+## Technologies
+
+- HTML/CSS: Used for structuring and styling the user interface.
+- JavaScript: Implemented the countdown functionality and user interactions.
+- Local Storage: Utilized for storing and retrieving countdown data between sessions.
+
+---
+
+_Note: This document provides an overview of Custom Countdown. For detailed instructions and more information, please refer to the source code documentation._
--- script.js
+++ script.js
@@ -11,20 +11,41 @@ const completeEl = document.getElementById('complete');
 const completeElInfo = document.getElementById('complete-info');
 const completeBtn = document.getElementById('complete-button');
 
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+const COUNTDOWN_ENDED = 0;
+
 let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
 let countdownActive;
 let savedCountdown;
 
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
-
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+function showCountdown(days, hours, minutes, seconds) {
+  // Populate countdown
+  countdownElTitle.textContent = `${countdownTitle}`;
+  timeElements[0].textContent = `${days}`;
+  timeElements[1].textContent = `${hours}`;
+  timeElements[2].textContent = `${minutes}`;
+  timeElements[3].textContent = `${seconds}`;
+
+  // Adjust visibility
+  completeEl.hidden = true;
+  countdownEl.hidden = false;
+}
+
+function showCountdownComplete() {
+  countdownEl.hidden = true;
+  clearInterval(countdownActive);
+  completeElInfo.textContent = `${countdownTitle} finished on ${countdownDate}`;
+  completeEl.hidden = false;
+}
 
 // Populate Countdown / Complete UI
 function updateDOM() {
@@ -32,31 +53,21 @@ function updateDOM() {
     const now = new Date().getTime();
     const distance = countdownValue - now;
 
-    const days = Math.floor(distance / day);
-    const hours = Math.floor((distance % day) / hour);
-    const minutes = Math.floor((distance % hour) / minute);
-    const seconds = Math.floor((distance % minute) / second);
+    const days = Math.floor(distance / DAY);
+    const hours = Math.floor((distance % DAY) / HOUR);
+    const minutes = Math.floor((distance % HOUR) / MINUTE);
+    const seconds = Math.floor((distance % MINUTE) / SECOND);
 
     // Hide Input
     inputContainer.hidden = true;
 
     // If the countdown has ended, show complete
-    if (distance < 0) {
-      countdownEl.hidden = true;
-      clearInterval(countdownActive);
-      completeElInfo.textContent = `${countdownTitle} finished on ${countdownDate}`;
-      completeEl.hidden = false;
+    if (distance < COUNTDOWN_ENDED) {
+      showCountdownComplete();
     } else {
-      // Else, show countdown in progress
-      countdownElTitle.textContent = `${countdownTitle}`;
-      timeElements[0].textContent = `${days}`;
-      timeElements[1].textContent = `${hours}`;
-      timeElements[2].textContent = `${minutes}`;
-      timeElements[3].textContent = `${seconds}`;
-      completeEl.hidden = true;
-      countdownEl.hidden = false;
+      showCountdown(days, hours, minutes, seconds);
     }
-  }, second);
+  }, SECOND);
 }
 
 // Take Values from Form Input
@@ -72,13 +83,11 @@ function updateCountdown(e) {
 
   localStorage.setItem('countdown', JSON.stringify(savedCountdown));
 
-  // Check for valid date
-  if (countdownDate === '') {
-    alert('Please select a date for the countdown');
-  } else {
-    // Get number version of current Date, updateDOM
+  if (countdownDate) {
     countdownValue = new Date(countdownDate).getTime();
     updateDOM();
+  } else {
+    alert('Please select a date for the countdown.');
   }
 }
 
@@ -93,6 +102,9 @@ function reset() {
   // Reset values
   countdownTitle = '';
   countdownDate = '';
+
+  countdownForm.reset();
+
   localStorage.removeItem('countdown');
 }
 
